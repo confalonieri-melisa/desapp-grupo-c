5d99e3ec072fd973266d830d85ddef8907ec6758
Candela <candelamarzaroli@gmail.com>
2026-09-16 23:57:39 -0300

    version 2.0

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,29 @@
+import {ArrowRight} from "lucide-react";
+
 export default function Home() {
   return (
     <div className="bg">
       <form className="card">
-        <h1>LOGIN</h1>
-        <div className="form-inputs">
+        <div className="card-description"><h1 className="card-title">¡Bienvenida{" "}<span className="card-title-span"> de vuelta!</span></h1>
+          <p className="card-text">Inicia sesion para continuar explorando el mercado de valoracion de jugadores.</p>
+        </div><div className="form-inputs">
           <div className="input-wrapper">
             <input type='text' id='input' required></input>
             <label
                 className='placeholder'>
-              Username
+              Nombre de usuario
             </label>
           </div>
           <div className="input-wrapper">
             <input type='password' id='password' required></input>
             <label
                 className='placeholder'>
-              Password
+              Contraseña
             </label>
           </div>
         </div>
-        <button type="submit">Login</button>
-        <p>No tienes cuenta? <a href="/">Regístrate</a></p>
+        <button type="submit" className="login-btn">Iniciar sesión <ArrowRight /> </button>
+        <p className="action-text">¿No tienes cuenta? <a className="action-link" href="/">Regístrate</a></p>
       </form>
     </div>
   );
