966ccbca1a4bbd52b1601a4230728ae6bcbdf24b
Candela <candelamarzaroli@gmail.com>
2026-09-22 22:17:33 -0300

    input con handleo de errores 🍒💣

--- src/components/FormInput.tsx
+++ src/components/FormInput.tsx
@@ -1,14 +1,25 @@
+import type { InputHTMLAttributes } from 'react';
 import styles from './FormInput.module.scss';
 
-type FormInputProps = { type: string; label: string };
+type FormInputProps = InputHTMLAttributes<HTMLInputElement> & {
+    label: string;
+    error?: string;
+};
 
-export default function FormInput({ type, label }: FormInputProps) {
+export default function FormInput({ type = 'text', label, error, ...props }: FormInputProps) {
     return (
-        <div className={styles['input-wrapper']}>
-            <input type={type} required />
-            <label className={styles.placeholder}>
-                {label}
-            </label>
+        <div className={styles.container}>
+            <div className={`${styles['input-wrapper']} ${error ? styles['has-error'] : ''}`}>
+                <input type={type} placeholder=" " {...props} />
+                <label className={styles.placeholder}>
+                    {label}
+                </label>
+            </div>
+            {error && (
+                <span className={styles['error-text']}>
+                    {error}
+                </span>
+            )}
         </div>
     );
 }
