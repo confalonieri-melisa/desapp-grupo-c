7a7a499f3c6ffe15c0cb2b59cd9d775ec46a5ff8
Candela <candelamarzaroli@gmail.com>
2026-09-22 22:01:15 -0300

    root pelado apra caer en algun lado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,39 @@
-import { redirect } from "next/navigation";
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuth } from '@/context/AuthContext';
+import ActionButton from '@/components/ActionButton';
+import Card, { CardTitle, CardTitleSpan } from '@/components/Card';
 
 export default function Home() {
-  redirect("/login");
+  const router = useRouter();
+  const { user, isAuthenticated, logout } = useAuth();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      router.push('/login');
+    }
+  }, [isAuthenticated, router]);
+
+  if (!isAuthenticated || !user) {
+    return null;
+  }
+
+  return (
+    <Card>
+        <CardTitle>
+          ¡Bienvenida, <CardTitleSpan>{user.name}</CardTitleSpan>!
+        </CardTitle>
+
+      <ActionButton
+        text="Cerrar sesión"
+        type="button"
+        onClick={() => {
+          logout();
+          router.push('/login');
+        }}
+      />
+    </Card>
+  );
 }
